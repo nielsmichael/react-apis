c002523567246adbaa0617a3761953fa2d2447d3
xXnielsXx <nielsmichael@comcast.net>
2022-07-01 09:01:56 +0900

    Simplify code to use explicit promises

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/').then(res => {
-      return res.json();
-    }).then(data => {
-      // transform data titles to match props in MoviesList component
-      const transformedMovies = data.results.map(movieData => {
-        // return new object
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      });
-      // return transformed movies instead of just data.results
-      setMovies(transformedMovies);
-    })
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/"); // get request
+    // returns promise
+    const data = await response.json();
+
+    // transform data titles to match props in MoviesList component
+    const transformedMovies = data.results.map((movieData) => {
+      // return new object
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    // return transformed movies instead of just data.results
+    setMovies(transformedMovies);
   }
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
 
   return (
     <React.Fragment>
